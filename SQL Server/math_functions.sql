bbbfb6a20a6fd9ab391d73f996258f0e9851b060
nuthanmurarysetty <inbox.nuthan@gmail.com>
2024-08-16 23:53:39 +0530

    Added math functions

--- SQL Server/math_functions.sql
+++ SQL Server/math_functions.sql
@@ -3,3 +3,27 @@ Select Abs(123) as whole_number, Abs(-123) as negative_whole_number, Abs(123.12)
 whole_number	negative_whole_number	decimal_number	decimal_to_round_next
 123				123						123.12			123.66
 */
+
+Select CEILING(1.23) as it_gives_two, CEILING(1.9) as it_gives_two
+/*
+it_gives_two	it_gives_two
+2				2
+*/
+
+Select Floor(3.23) as it_gives_three, Floor(2.9) as it_gives_two
+/*
+it_gives_three	it_gives_two
+3				2
+*/
+
+Select Square(2) as two_square
+/*
+two_square
+4
+*/
+
+Select SQRT(4) as square_root_for_4_is_2
+/*
+square_root_for_4_is_2
+2
+*/
