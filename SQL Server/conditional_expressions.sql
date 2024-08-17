267155adfd996456e90af1bdf2a3f50bac596c61
nuthanmurarysetty <inbox.nuthan@gmail.com>
2024-08-17 23:24:18 +0530

    Update with IFNULL_vs_ISNULL

--- SQL Server/conditional_expressions.sql
+++ SQL Server/conditional_expressions.sql
@@ -9,3 +9,5 @@ SELECT IIF(@total_population > 30000000, 'High','Low')
 -- This format we called as Searched CASE expression
 Declare @total_population_case bigint = 2500000;
 SELECT CASE WHEN @total_population_case > 30000000 THEN 'High' ELSE 'LOW' END
+
+SELECT ISNULL(null,'becuase first parameter is null')
--- SQLite/conditional_expressions.sql
+++ SQLite/conditional_expressions.sql
@@ -4,4 +4,8 @@ SELECT IIF(2500000000 > 30000000, 'High','Low')
 -- CASE - Alternative to IIF and this is simpler
 -- CASE Selector_Expression/Column WHEN 'Value/match_expression' THEN 'True value' ELSE 'False Value' END
 -- Note: CASE executing from top to bottom it will start checking if first occurence then case statement terminates
+
+-- This format we called as Searched CASE expression
 SELECT CASE WHEN 250000 > 30000000 THEN 'High' ELSE 'LOW' END
+
+SELECT IFNULL('hi','becuase first parameter is null')
